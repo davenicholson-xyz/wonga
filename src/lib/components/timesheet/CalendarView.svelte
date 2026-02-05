f7a2b3413f8bb14a1dedafdaccf6b27aba1a0584
Dave Nicholson <me@davenicholson.xyz>
2026-02-05 17:44:02 +0000

    alignment fix

--- src/lib/components/timesheet/CalendarView.svelte
+++ src/lib/components/timesheet/CalendarView.svelte
@@ -131,25 +131,27 @@
 				}}
 			>
 				<span class="leading-none {isToday ? 'text-primary font-bold' : ''}">{cell.day}</span>
-				{#if cell.entry?.unavailable}
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 16 16"
-						fill="currentColor"
-						class="w-4 h-4 shrink-0 text-error/50"
-					>
-						<path
-							d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z"
-						/>
-					</svg>
-				{:else}
-					<span
-						class="w-1.5 h-1.5 shrink-0 rounded-full mt-0.5"
-						class:bg-warning={cell.entry?.start_time === '06:00'}
-						class:bg-info={cell.entry && cell.entry.start_time !== '06:00'}
-						class:invisible={!cell.entry}
-					></span>
-				{/if}
+				<span class="flex items-center justify-center w-4 h-4 shrink-0">
+					{#if cell.entry?.unavailable}
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							viewBox="0 0 16 16"
+							fill="currentColor"
+							class="w-4 h-4 text-error/50"
+						>
+							<path
+								d="M5.28 4.22a.75.75 0 0 0-1.06 1.06L6.94 8l-2.72 2.72a.75.75 0 1 0 1.06 1.06L8 9.06l2.72 2.72a.75.75 0 1 0 1.06-1.06L9.06 8l2.72-2.72a.75.75 0 0 0-1.06-1.06L8 6.94 5.28 4.22Z"
+							/>
+						</svg>
+					{:else}
+						<span
+							class="w-1.5 h-1.5 rounded-full"
+							class:bg-warning={cell.entry?.start_time === '06:00'}
+							class:bg-info={cell.entry && cell.entry.start_time !== '06:00'}
+							class:invisible={!cell.entry}
+						></span>
+					{/if}
+				</span>
 			</button>
 		{/each}
 	{/each}
